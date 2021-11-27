fb01d4f60b11bcf1e617646b4744de11cdd3c8bf
casa92 <casartelli92@gmail.com>
2021-11-27 18:10:11 +0100

    snack 1

---
+++ snack1/js/script.js
@@ -0,0 +1,58 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal nel DOM
+
+
+//creo array di oggetti
+const bikesArray =
+[
+    {
+        modello: 'prova1',
+        peso: 10
+    },
+
+    {
+        modello: 'prova2',
+        peso: 20
+    },
+
+    {
+        modello: 'prova3',
+        peso: 30
+    }
+];
+
+//creo variabile a cui assegnare valore ad ogni ciclo
+let bikeWeightier = bikesArray[0];
+
+//creo ciclo per accedere a tutti gli oggetti presenti nell'array
+for ( i = 0; i < bikesArray.length; i++ ) {
+    let thisBike = bikesArray[i];
+    console.log(thisBike);
+
+    //ad ogni ciclo, se l'oggetto avrà la chiave 'peso' maggiore, verrà inserite nella variabile dichiarata prima del ciclo    if (thisBike.peso > bikeWeightier.peso) {
+        bikeWeightier = thisBike;
+    }
+}
+
+//destrutturo l'oggetto 
+const {modello, peso} = bikeWeightier;
+
+console.log(peso);
+
+//selezione elemento del dom 
+let userMessageDom = document.getElementById('user-message');
+
+//creo output per utente
+let userMessage = `
+    <h2>Il modello con peso maggiore è ${modello}, con un peso totale di ${peso} Kg.</h2>
+`;
+
+//concateno messaggio creato all'elemtento del dom selezionato
+userMessageDom.innerHTML += userMessage;
+
+
+
+
+
